6b08d157aab3bccd21d2ebeadaeeb62a52ff805e
timkrauth13 <timkrauth7@gmail.com>
2023-05-29 21:56:03 +0200

    save

--- Stardew/code/main.py
+++ Stardew/code/main.py
@@ -9,7 +9,7 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        pygame.display.set_caption('Sprout land')
+        pygame.display.set_caption('Stardew Valley 2 für Meli')
         self.clock = pygame.time.Clock()
         self.level = Level()
 
